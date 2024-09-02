ce6e09a3673acf200b7ea0a7ac88a621181371d9
lucassamelFadami <lucas.samel@fadami.com.br>
2024-09-02 17:33:28 -0300

    Access DataBase via ADO Net

--- Program.cs
+++ Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Microsoft.Data.SqlClient;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        const string connectionString = "Server=localhost,1433;Database=balta;User ID=sa;Password=1q2w3e4r@#$;TrustServerCertificate=True";
+
+        using ( var connection = new SqlConnection(connectionString))
+        {
+            connection.Open();           
+
+            using (var command = new SqlCommand())
+            {
+                command.Connection = connection;
+                command.CommandType = System.Data.CommandType.Text;
+                command.CommandText = "SELECT [Id], [Title] FROM [Category]";
+
+                var reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Console.WriteLine($"{reader.GetGuid(0)} - {reader.GetString(1)}");
+                }
+            }
+        }
+
+        Console.ReadKey();
+    }
+}
+
+
