2a07075bba2759554cf9d39ea13053534a862d7b
lucassamelFadami <lucas.samel@fadami.com.br>
2024-09-02 21:37:35 -0300

    Insert and Update using Dapper

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Dapper;
+using dapper_data_access.Models;
 using Microsoft.Data.SqlClient;
 
 class Program
@@ -7,26 +9,80 @@ class Program
     {
         const string connectionString = "Server=localhost,1433;Database=balta;User ID=sa;Password=1q2w3e4r@#$;TrustServerCertificate=True";
 
+        
+        
+
         using ( var connection = new SqlConnection(connectionString))
         {
-            connection.Open();           
+            UpdateCategory(connection);
 
-            using (var command = new SqlCommand())
-            {
-                command.Connection = connection;
-                command.CommandType = System.Data.CommandType.Text;
-                command.CommandText = "SELECT [Id], [Title] FROM [Category]";
+            ListCategories(connection);
 
-                var reader = command.ExecuteReader();
-
-                while (reader.Read())
-                {
-                    Console.WriteLine($"{reader.GetGuid(0)} - {reader.GetString(1)}");
-                }
-            }
+            //CreateCategory(connection);           
+            
         }
 
         Console.ReadKey();
+    }
+
+    static void ListCategories(SqlConnection connection)
+    {
+        var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
+        foreach (var item in categories)
+        {
+            Console.WriteLine($"{item.Id} - {item.Title}");
+        }
+    }
+
+    static void CreateCategory(SqlConnection connection)
+    {
+        var category = new Category
+        {
+            Id = Guid.NewGuid(),
+            Title = "Amazon AWS",
+            Url = "amazon",
+            Description = "Categoria destinada a serviços AWS",
+            Order = 8,
+            Summary = "AWS Cloud",
+            Featured = false
+        };
+
+        var insertSql = @"INSERT INTO
+            [Category]
+        VALUES(
+            @Id,
+            @Title,
+            @Url,
+            @Summary,
+            @Order,
+            @Description,
+            @Featured)";
+
+        var rows = connection.Execute(insertSql, new
+        {
+            category.Id,
+            category.Title,
+            category.Url,
+            category.Summary,
+            category.Order,
+            category.Description,
+            category.Featured
+        });
+
+        Console.WriteLine($"{rows} linhas inseridas ");
+    }
+
+    static void UpdateCategory(SqlConnection connection)
+    {
+        var updateQuery = "UPDATE [Category] SET [Title] = @title WHERE [Id] = @id";
+
+        var rows = connection.Execute(updateQuery, new
+        {
+            id = new Guid("bd9eb6e2-7cad-4eae-8e4c-d75bdbfd1498"),
+            title = "Dapper 2024"
+        });
+
+        Console.WriteLine($"{rows} registros atualizadas");
     }
 }
 
