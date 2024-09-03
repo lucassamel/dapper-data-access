cf7921fbe9b89ce265b8cf19bdbb39782c3c4cb5
lucassamelFadami <lucas.samel@fadami.com.br>
2024-09-03 19:30:12 -0300

    Read View

--- Program.cs
+++ Program.cs
@@ -221,6 +221,17 @@ class Program
         });
         Console.WriteLine($"A categoria inserida foi: {id}");
     }
+    static void ReadView(SqlConnection connection)
+    {
+        var sql = "SELECT * FROM [vwCourses]";
+        var courses = connection.Query(sql);
+
+        foreach (var item in courses)
+        {
+            Console.WriteLine($"{item.Id} - {item.Title}");
+        }
+    }
+
 }
 
 
