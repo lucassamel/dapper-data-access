b1f6cc931e029db2b56fca0617db21f71fb498f4
lucassamelFadami <lucas.samel@fadami.com.br>
2024-09-03 19:26:10 -0300

    Execute Scalar,; OUTPUT inserted.[Id]

--- Program.cs
+++ Program.cs
@@ -186,6 +186,41 @@ class Program
             Console.WriteLine(item.Title);
         }
     }
+    static void ExecuteScalar(SqlConnection connection)
+    {
+        var category = new Category();
+        category.Title = "Amazon AWS";
+        category.Url = "amazon";
+        category.Description = "Categoria destinada a serviços do AWS";
+        category.Order = 8;
+        category.Summary = "AWS Cloud";
+        category.Featured = false;
+
+        var insertSql = @"
+                INSERT INTO 
+                    [Category] 
+                OUTPUT inserted.[Id]
+                VALUES(
+                    NEWID(), 
+                    @Title, 
+                    @Url, 
+                    @Summary, 
+                    @Order, 
+                    @Description, 
+                    @Featured) 
+                ";        
+
+        var id = connection.ExecuteScalar<Guid>(insertSql, new
+        {
+            category.Title,
+            category.Url,
+            category.Summary,
+            category.Order,
+            category.Description,
+            category.Featured
+        });
+        Console.WriteLine($"A categoria inserida foi: {id}");
+    }
 }
 
 
