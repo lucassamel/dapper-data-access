f3a077ea75403f5e2d09059104894e975fa48b7f
lucassamelFadami <lucas.samel@fadami.com.br>
2024-09-03 20:35:13 -0300

    One To Many

--- Program.cs
+++ Program.cs
@@ -20,7 +20,8 @@ class Program
             //ExecuteProcedure(connection);
             //ExecuteReadProcedure(connection);
             //ReadView(connection);
-            OneToOne(connection);
+            //OneToOne(connection);
+            OneToMany(connection);
 
             //ListCategories(connection);
         }
@@ -254,6 +255,50 @@ class Program
         foreach (var item in items)
         {
             Console.WriteLine($"{item.Title} - Curso: {item.Course.Title}");
+        }
+    }
+    static void OneToMany(SqlConnection connection)
+    {
+        var sql = @"
+                SELECT 
+                    [Career].[Id],
+                    [Career].[Title],
+                    [CareerItem].[CareerId],
+                    [CareerItem].[Title]
+                FROM 
+                    [Career] 
+                INNER JOIN 
+                    [CareerItem] ON [CareerItem].[CareerId] = [Career].[Id]
+                ORDER BY
+                    [Career].[Title]";
+
+        var careers = new List<Career>();
+        var items = connection.Query<Career, CareerItem, Career>(
+            sql,
+            (career, item) =>
+            {
+                var car = careers.Where(x => x.Id == career.Id).FirstOrDefault();
+                if (car == null)
+                {
+                    car = career;
+                    car.Items.Add(item);
+                    careers.Add(car);
+                }
+                else
+                {
+                    car.Items.Add(item);
+                }
+
+                return career;
+            }, splitOn: "CareerId");
+
+        foreach (var career in careers)
+        {
+            Console.WriteLine($"{career.Title}");
+            foreach (var item in career.Items)
+            {
+                Console.WriteLine($" - {item.Title}");
+            }
         }
     }
 
